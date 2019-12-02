cf044d92e1f8f56deba8821016244ad6569e37b2
Ismael <ihdominguez905@gmail.com>
2019-12-01 20:15:03 -0400

    Add System.ComponentModel.Annotations package to Northwind.Core project.
     - Add entity Order.cs.
     - Add DataAnnotations to Entity Customer.cs.

--- src/Northwind.Core/Entities/Northwind/Customer.cs
+++ src/Northwind.Core/Entities/Northwind/Customer.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Core.Entities.Northwind
 {
     public class Customer
     {
+        [MaxLength(5)]
         public string CustomerID { get; set; }
+        [MaxLength(40)]
         public string CompanyName { get; set; }
+        [MaxLength(30)]
         public string ContactName { get; set; }
+        [MaxLength(30)]
         public string ContactTitle { get; set; }
+        [MaxLength(60)]
         public string Address { get; set; }
+        [MaxLength(15)]
         public string City { get; set; }
+        [MaxLength(15)]
         public string Region { get; set; }
+        [MaxLength(10)]
         public string PostalCode { get; set; }
+        [MaxLength(15)]
         public string Country { get; set; }
+        [MaxLength(24)]
         public string Phone { get; set; }
+        [MaxLength(24)]
         public string Fax { get; set; }
     }
 }
